b7b0a3dd1027ebb05383114719df71f38eb51af8
rajahwu <vmradford@gmail.com>
2022-10-11 19:00:55 -0400

    complete recursion pset problem

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -11,10 +11,17 @@ addToTwelve([1, 11, 4, 7, 6]); // true
 addToTwelve([1, 12, 4, 7, 6]); // false
 addToTwelve([1]); // false
 ***********************************************************************/
-
+// ? Understand the problem
+// * Input : array
+// * Return: boolean
 
 function addToTwelve(arr) {
   // Your code here
+   if(arr.length === 0) return false  // * base case 
+let num1 = arr.pop() // * recursive step    
+let num2 = arr[arr.length - 1]       // * recursive case arr.length !== 0
+  if(num1 + num2 === 12) return true
+  return addToTwelve(arr)
 }
 
 
